5d7d16069204005c800ebeb14e5957bd5e0d5a24
Shashwat Srivastava <113192360+Shashwat4557@users.noreply.github.com>
2024-07-21 05:23:10 +0530

    UPLOADED

---
+++ blogging/gallery/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Product, User, Review
+
+admin.site.register(Product)
+admin.site.register(User)
+admin.site.register(Review)
---
+++ blogging/gallery/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login/', views.login_page, name='login'),
+    path('register/', views.register_page, name='register'),
+    path('logout/', views.logout_view, name='logout'),
+    path('home/', views.product_list, name='product_list'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('product/<int:pk>/edit/', views.edit_product, name='edit_product'),
+    path('product/<int:pk>/delete/', views.delete_product, name='delete_product'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('product/<int:pk>/add_review/', views.add_review, name='add_review'),
+]
